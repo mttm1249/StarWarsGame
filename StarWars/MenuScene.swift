9aa3a87997c6b1645d5fa0b0567ef0bee944b2e6
mttm1249 <87662841+mttm1249@users.noreply.github.com>
2021-09-03 10:16:56 +0300

    24 lesson (menu scene)

---
+++ StarWars/MenuScene.swift
@@ -0,0 +1,33 @@
+//
+//  MenuScene.swift
+//  StarWars
+//
+//  Created by Денис on 03.09.2021.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
+        let texture = SKTexture(imageNamed: "play")
+        let button = SKSpriteNode(texture: texture)
+        button.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        button.name = "runButton"
+        self.addChild(button)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: self)
+        let nove = self.atPoint(location)
+        
+        if nove.name == "runButton" {
+            
+            let transition = SKTransition.crossFade(withDuration: 1.0)
+            let gameScene = GameScene(size: self.size)
+            gameScene.scaleMode = .aspectFill
+            self.scene!.view?.presentScene(gameScene, transition: transition)
+        }
+    }
+}
